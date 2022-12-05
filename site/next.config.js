8fb7497652cf11c2b90c7417f770556b67550c97
KodeStar <kodestar@gmail.com>
2022-12-05 16:09:17 +0000

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,15 +2,15 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
+    name: 'Something New',
     description: 'The official store for Kasm supported applications.',
     icon: '/img/logo.svg',
-    listUrl: 'https://apps.kasmweb.com/list.json',
+    listUrl: 'https://kodestar.github.io/somethingnew/list.json',
     contactUrl: 'https://kasmweb.com/support',
   },
   reactStrictMode: true,
   swcMinify: true,
-  basePath: '/kasm-apps',
+  basePath: '/somethingnew',
   trailingSlash: true,
   images: {
     unoptimized: true,
